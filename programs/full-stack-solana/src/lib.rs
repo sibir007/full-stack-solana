f22849919c8ef0550fc505529a1dd216c10a3bd1
Dmitry Sibiryakov <sibiriakoff2006@yandex.ru>
2025-08-09 21:55:15 +0300

    cpi test2

--- programs/full-stack-solana/src/lib.rs
+++ programs/full-stack-solana/src/lib.rs
@@ -4,13 +4,42 @@ declare_id!("HBq27XcCHHCyXbaVwacJyk6DpDTKfa2AnrcDrYEr48x");
 
 #[program]
 pub mod full_stack_solana {
+    use anchor_lang::solana_program::entrypoint::ProgramResult;
+
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn create(ctx: Context<Create>) -> ProgramResult {
+        let base_account = &mut ctx.accounts.my_account;
+        base_account.count = 0;
+        msg!("Creating a new account with the provided data");
         Ok(())
     }
+
+    pub fn increment(ctx: Context<Increment>) -> ProgramResult {
+        msg!("Incrementing the value in the account");
+        Ok(())
+    }
+    
+
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Create<'info> {
+    #[account(init, payer = user, space = 8 + 32)]
+    pub my_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Increment<'info> {
+    #[account(mut)]
+    pub my_account: Account<'info, BaseAccount>,
+}
+
+#[account]
+pub struct BaseAccount {
+    pub count: u64,
+}
+
